29006b39f5cc3bd14ad7bfa3b9c6480699a5c38e
anil_akbay <anilakbay20@gmail.com>
2024-11-25 22:30:33 +0300

    İlk commit - BaseEntity ve Product entity sınıfları oluşturuldu

---
+++ src/main/java/com/example/shopsystem/model/Product.java
@@ -0,0 +1,43 @@
+package com.example.shopsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Column;
+
+@Entity
+public class Product extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    // Getter ve Setter metodları
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
+
